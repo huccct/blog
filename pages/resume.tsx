03fd7be156bdb9a1a824f2b7458e87ea4a4167b6
huccct <2972223145@qq.com>
2024-05-24 14:48:28 +0800

    chore(fe): update resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -2,7 +2,7 @@
 import siteMetadata from '@/data/siteMetadata'
 import { PageSEO } from '@/components/SEO'
 import projectsData from '@/data/projectsData'
-import ResumePDF from 'pages/ResumePDF'
+import ResumePDF from './ResumePDF'
 import { pdf } from '@react-pdf/renderer'
 
 export default function Resume() {
